5a8dfbd950a1875ae5b30d5fdb6c101191374561
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-01-13 13:47:57 -0600

    feat: añade archivos de db y rutas al componente app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import connectDB from "./db/conection";
+import routes from "./routes/userRoutes";
 
 const app = express();
+
+//middleware para conectar a la base de datos
+connectDB();
+
+app.use(routes);
 
 app.get("/", (_req, res) => {
 	res.send("Hello World!");

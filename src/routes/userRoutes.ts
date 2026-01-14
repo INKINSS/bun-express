91aa5e765d54079780db008b95a7449f86a5c59a
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-01-13 20:00:50 -0600

    fix: corrige error de middleware a controller para users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUsers } from "../middlewares/userMiddlwares";
+import { getUsers } from "../controllers/userControllers";
 
 const router = Router();
 

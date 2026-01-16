2dca1bec615bcf5fe2da9c7609b32c655afbf1a3
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-01-16 10:03:48 -0600

    chore(config): especifica la db por defecto, muestra la db conectada

--- src/db/conection.ts
+++ src/db/conection.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(
-			`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/?authSource=admin`,
+			`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/test?authSource=admin`,
 		);
 		console.log(`MongoDB connected: ${conn.connection.host}`);
+		console.log(`Database name: ${conn.connection.name}`);
 	} catch (error) {
 		console.error(`Error connecting to MongoDB: ${error as string}`);
 		process.exit(1);

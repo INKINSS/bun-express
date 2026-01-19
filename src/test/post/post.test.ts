da3607e2201e7fc34922d540be06059ca74c7f43
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-01-19 10:37:43 -0600

    test(test): :test_tube: añade test principales para la entidad post

---
+++ src/test/post/post.test.ts
@@ -0,0 +1,80 @@
+import { afterAll, beforeAll, describe, expect, test } from "bun:test";
+import { PostModel } from "../../schemas/postSchema";
+import {
+	createPostService,
+	getPostsService,
+} from "../../services/postServices";
+import { connectMemoryDB, disconnectMemoryDB } from "../setup/mongoMemory";
+
+const samplePost = [
+	{
+		title: "First Post",
+		author: "Alice",
+		contents: "This is the content of the first post.",
+		tags: ["introduction", "welcome"],
+	},
+	{
+		title: "Second Post",
+		author: "Bob",
+		contents: "This is the content of the second post.",
+		tags: ["update", "news"],
+	},
+	{
+		title: "Third Post",
+		author: "Charlie",
+		contents: "This is the content of the third post.",
+		tags: ["announcement", "events"],
+	},
+];
+
+beforeAll(async () => {
+	await connectMemoryDB();
+	await PostModel.deleteMany({});
+	for (const post of samplePost) {
+		const newPost = new PostModel(post);
+		await newPost.save();
+	}
+});
+
+afterAll(async () => {
+	await disconnectMemoryDB();
+});
+
+describe("get all Posts", () => {
+	test("should retrieve all posts successfully", async () => {
+		const post = await getPostsService();
+		expect(post.length).toEqual(samplePost.length);
+	});
+});
+
+describe("create a new Post", () => {
+	test("should create a new post successfully", async () => {
+		const post = await createPostService({
+			title: "Test Post",
+			author: "John Doe",
+			contents: "This is a test post content",
+			tags: ["test", "post"],
+		});
+
+		expect(post.title).toBe("Test Post");
+		expect(post.author).toBe("John Doe");
+	});
+
+	test("should fail to create a post with missing title", async () => {
+		expect(
+			createPostService({
+				author: "Jane Doe",
+				contents: "This post has no title",
+				tags: ["invalid", "post"],
+			}),
+		).rejects.toThrow();
+	});
+
+	test("should fail to create a post with minimal data", async () => {
+		expect(
+			createPostService({
+				title: "No title and author",
+			}),
+		).rejects.toThrow();
+	});
+});

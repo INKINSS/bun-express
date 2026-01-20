10da4a30248a4e9c76d9e0467b3142a64753df03
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-01-19 21:13:56 -0600

    feat(services): añade tipos para servicios de post
    añade metodo GET para un post, UPDATE, DELETE

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -1,11 +1,31 @@
 // services/postService.ts
 import { PostModel } from "../schemas/postSchema";
 
+type PostData = {
+	_id?: string;
+	title: string;
+	author?: string;
+	contents?: string;
+	tags?: string[];
+};
+
 export const getPostsService = async () => {
 	return await PostModel.find().sort();
 };
 
-export const createPostService = async (data: any) => {
+export const createPostService = async (data: PostData) => {
 	const post = new PostModel(data);
 	return await post.save();
+};
+
+export const getPostByIdService = async (id: string) => {
+	return await PostModel.findById(id);
+};
+
+export const updatePostService = async (id: string, data: PostData) => {
+	return await PostModel.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deletePostService = async (id: string) => {
+	return await PostModel.findByIdAndDelete(id);
 };

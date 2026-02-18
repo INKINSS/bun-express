17462356d82f61ddce2f81a04b8a2e24d77df261
Martín Samuel Pérez Montenegro <martinsamuelpp@gmail.com>
2026-02-18 16:00:59 -0600

    feat(controllers): añade CRUD por defecto para el schema de users

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,12 +1,57 @@
 import type { Request, Response } from "express";
-import { UserModel } from "../schemas/userSchema";
+import {
+	createUserService,
+	deleteUserService,
+	getUserByIdService,
+	getUsersService,
+	updateUserService,
+} from "../services/userServices";
 
 export const getUsers = async (_req: Request, res: Response) => {
 	try {
-		const users = await UserModel.find();
+		const users = await getUsersService();
 		res.send(users);
 	} catch (error) {
-		console.error(`Error getting users: ${error as string}`);
-		throw error;
+		console.log("error", error);
+	}
+};
+
+export const getUserById = async (_req: Request, res: Response) => {
+	try {
+		const userById = await getUserByIdService(_req.params.id as string);
+		res.send(userById);
+	} catch (error) {
+		console.log("error", error);
+	}
+};
+
+export const createUser = async (_req: Request, res: Response) => {
+	try {
+		const { username, fullname, age } = _req.body;
+		const user = await createUserService({ username, fullname, age });
+		res.send(user);
+	} catch (error) {
+		console.log("error", error);
+	}
+};
+
+export const updateUser = async (_req: Request, res: Response) => {
+	try {
+		const updateUser = await updateUserService(
+			_req.params.id as string,
+			_req.body,
+		);
+		res.send(updateUser);
+	} catch (error) {
+		console.log(`error updating user: ${error as string}`);
+	}
+};
+
+export const deleteUser = async (_req: Request, res: Response) => {
+	try {
+		const deleteUser = await deleteUserService(_req.params.id as string);
+		res.send(deleteUser);
+	} catch (error) {
+		console.log("error to deleted user", error);
 	}
 };
